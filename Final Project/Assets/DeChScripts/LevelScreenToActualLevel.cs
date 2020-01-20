610ad6fef5c022718efad032794bc2edea3aea20
dchang6 <42654029+dchang6@users.noreply.github.com>
2020-01-20 15:46:42 -0500

    x

---
+++ Final Project/Assets/DeChScripts/LevelScreenToActualLevel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Threading;
+using UnityEngine.SceneManagement;
+
+public class LevelScreenToActualLevel : MonoBehaviour
+{
+    public float Timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Timer = 0.0f;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Timer -= Time.deltaTime;
+
+        if(Timer <=-3)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            Time.timeScale = 0;
+        }
+        
+    }
+}
---
+++ Final Project/Assets/DeChScripts/StorytoMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Threading;
+using UnityEngine.SceneManagement;
+
+public class StorytoMenu : MonoBehaviour
+{
+    public float PTimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PTimer = 0.0f;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        PTimer -= Time.deltaTime;
+
+        if (PTimer <= -20)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            Time.timeScale = 0;
+        }
+
+    }
+}
