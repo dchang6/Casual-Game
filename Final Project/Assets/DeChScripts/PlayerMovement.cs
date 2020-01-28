2310fd146f125df80f2ab10484a9bb9c0c8c40dc
Dexavier <42654029+dchang6@users.noreply.github.com>
2020-01-28 15:10:19 -0500

    ddssd

---
+++ Final Project/Assets/DeChScripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed;
+
+    void Awake()
+    {
+    }
+
+    void FixedUpdate()
+    {
+        transform.Translate(speed * Time.deltaTime, 0, 0);
+    }
+}
