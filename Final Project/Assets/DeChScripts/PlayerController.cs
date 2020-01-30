b2e17239f2d082c7aced37b6e4c5d4ca3481f4d2
dchang6 <42654029+dchang6@users.noreply.github.com>
2020-01-30 12:39:53 -0500

    ss

---
+++ Final Project/Assets/DeChScripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+  public float jumpSpeed = 1f;
+  private float movement = 0f;
+  private Rigidbody2D rigidBody;
+
+  // Use this for initialization
+  void Start () {
+    rigidBody = GetComponent<Rigidbody2D> ();
+  }
+  
+  // Update is called once per frame
+  void Update () {
+    movement = Input.GetAxis ("Horizontal");
+    if(Input.GetKeyDown ("o")){
+      rigidBody.velocity = new Vector2(rigidBody.velocity.x,jumpSpeed);
+    }
+  }
+}
